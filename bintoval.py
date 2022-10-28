42c972530c8f976efc2253e99333230527485bfd
Siri N Shetty <114021638+siri-n-shetty@users.noreply.github.com>
2022-10-29 01:03:00 +0530

    Binary representation to base 10

---
+++ bintoval.py
@@ -0,0 +1,9 @@
+a = int(input("Enter leftmost digit "))
+b = int(input("Enter next digit "))
+c = int(input("Enter next digit "))
+d = int(input("Enter next digit "))
+a1 = (2**3)*a
+b1 = (2**2)*b
+c1 = (2**1)*c
+d1 = (2**0)*d
+print("The value is ",a1+b1+c1+d1)
